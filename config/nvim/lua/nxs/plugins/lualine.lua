c298f5e844522a7dc090f79175e90f79b65b0d03
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-10-09 11:35:12 +0900

    restructure

--- config/nvim/lua/nxs/plugins/lualine.lua
+++ config/nvim/lua/nxs/plugins/lualine.lua
@@ -14,19 +14,4 @@ return {
       },
     }
   },
-  { 
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
-    lazy = false,
-    event = { 'VeryLazy' },
-    dependencies = {
-      'nvim-tree/nvim-web-devicons'
-    },
-    keys = {
-      { '<leader>e', ':NvimTreeToggle<cr>', desc = 'Toggle File Explorer' }
-    },
-    config = function() 
-      require('nvim-tree').setup({})
-    end
-  },
 }
