16d3636126a39ebbf7bf176f4b9c1a2a6901ce54
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-10-15 14:33:31 +0900

    Add telescope

---
+++ config/nvim/lua/nxs/plugins/telescope.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    keys = {
+      {
+        "<leader><Tab>",
+        ":Telescope buffers<CR>",
+        mode = "n",
+        desc = "Open Telescope: Buffers"
+      },
+      -- Do I need this?
+      -- { "`g", ":Telescope git_files<CR>", mode = "n", desc = "Open Telescope: Git Files" },
+      {
+        "<leader>p",
+        ":Telescope find_files<CR>",
+        mode = "n",
+        desc = "Open Telescope: Find Files"
+      },
+      {
+        "<leader>F",
+        ":Telescope grep_string only_sort_text=true search=<CR>",
+        mode = "n",
+        desc = "Open Telescope: Find String"
+      },
+    },
+    init = function()
+      require("telescope").load_extension("fzf")
+    end
+  },
+}
