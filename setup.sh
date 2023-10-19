b1cc28c48a55b3b830b2712b36993e6e831dbe9d
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-10-19 22:17:46 +0900

    Add skhdrc config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-files=(bin zshrc nxs-omz tmux.conf zshenv config/nvim config/ranger gitconfig_global ideavimrc)
+files=(bin zshrc nxs-omz tmux.conf zshenv config/nvim config/ranger gitconfig_global ideavimrc skhdrc)
 
 for f in $files; do
   dest="$HOME/.$f"
