c2b6d6ef2a22d00da172dbe0b3f525edb25cc7f9
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-10-24 14:50:51 +0900

    Add nvim-ts-autotag

--- config/nvim/lua/nxs/plugins/treesitter.lua
+++ config/nvim/lua/nxs/plugins/treesitter.lua
@@ -1,12 +1,16 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      { "windwp/nvim-ts-autotag", },
+    },
     build = ':TSUpdate',
-    config = function ()
+    config = function()
       require('nvim-treesitter.configs').setup({
         ensure_installed = 'all',
         indent = { enable = true },
         highlight = { enable = true },
+        autotag = { enable = true },
       })
     end
   }
