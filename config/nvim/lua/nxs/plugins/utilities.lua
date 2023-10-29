7ac4a1427af85ccf4062428c9bb5cbca0ca3a0c1
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-10-29 23:19:24 +0900

    Remove easymotion

--- config/nvim/lua/nxs/plugins/utilities.lua
+++ config/nvim/lua/nxs/plugins/utilities.lua
@@ -1,22 +1,5 @@
 return {
   {
-    "easymotion/vim-easymotion",
-    event = "VeryLazy",
-    keys = {
-      {
-        "s",
-        "<Plug>(easymotion-overwin-f2)",
-        mode = "n",
-        desc = "EasyMotion",
-      }
-    },
-    init = function()
-      vim.g.EasyMotion_do_mapping = 0
-      vim.g.EasyMotion_smartcase = 1
-      vim.g.EasyMotion_use_migemo = 1
-    end
-  },
-  {
     "kylechui/nvim-surround",
     event = "VeryLazy",
     config = function()
