dcc15fa36b7b24347ae99dc7a7e08c6ec385d09d
Nas Villanueva <nxs@nxs.local>
2023-11-03 23:21:11 +0900

    Update lualine components

--- config/nvim/lua/nxs/plugins/lualine.lua
+++ config/nvim/lua/nxs/plugins/lualine.lua
@@ -7,6 +7,13 @@ return {
         component_separators = "",
         section_separators = "",
       },
+      sections = {
+        lualine_b = { "diagnostics" },
+        lualine_c = {
+          { "filename", path = 3 },
+        },
+        lualine_x = { "branch", "fileformat", "filetype" },
+      },
     },
   },
 }
