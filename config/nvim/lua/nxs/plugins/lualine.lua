4174aa82a87b962971fc657a1f5eaaca8237ed5a
Nas Villanueva <nxs@nxs.local>
2023-11-03 23:37:34 +0900

    Update lualine

--- config/nvim/lua/nxs/plugins/lualine.lua
+++ config/nvim/lua/nxs/plugins/lualine.lua
@@ -12,7 +12,31 @@ return {
         lualine_c = {
           { "filename", path = 3 },
         },
-        lualine_x = { "branch", "fileformat", "filetype" },
+        lualine_x = {
+          "branch",
+          {
+            function()
+              local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+              local msg = buf_ft
+              local clients = vim.lsp.get_active_clients()
+
+              if next(clients) == nil then
+                return msg
+              end
+
+              local active_lsp = {}
+              for _, client in ipairs(clients) do
+                local filetypes = client.config.filetypes
+                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                  table.insert(active_lsp, client.name)
+                end
+              end
+
+              return table.concat(active_lsp, ", ")
+            end,
+            icon = "",
+          },
+        },
       },
     },
   },
