5710b116be113937788256f39211b5b5d3cf7ab2
Nas Villanueva <nxs@nxs.local>
2023-11-03 23:13:19 +0900

    Add find in directory for nvim-tree

--- config/nvim/lua/nxs/plugins/explorer.lua
+++ config/nvim/lua/nxs/plugins/explorer.lua
@@ -6,6 +6,7 @@ return {
     lazy = false,
     dependencies = {
       "nvim-tree/nvim-web-devicons",
+      "nvim-telescope/telescope.nvim",
     },
     keys = {
       {
@@ -32,17 +33,54 @@ return {
         desc = "Explorer: Focus File",
       },
     },
-    opts = {
-      renderer = {
-        group_empty = true,
-      },
-      view = {
-        width = 40,
-      },
-    },
-    init = function()
+    config = function()
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
+
+      require("nvim-tree").setup({
+        renderer = {
+          group_empty = true,
+        },
+        view = {
+          width = 40,
+        },
+        on_attach = function(bufnr)
+          local nvim_tree = require("nvim-tree.api")
+
+          local function opts(desc)
+            return {
+              desc = "Explorer: " .. desc,
+              buffer = bufnr,
+              noremap = true,
+              silent = true,
+              nowait = true,
+            }
+          end
+
+          nvim_tree.config.mappings.default_on_attach(bufnr)
+
+          local function get_telescope_opts()
+            local node = nvim_tree.tree.get_node_under_cursor()
+            local is_folder = node.fs_stat and node.fs_stat.type == "directory"
+              or false
+            local basedir = is_folder and node.absolute_path
+              or vim.fn.fnamemodify(node.absolute_path, ":h")
+
+            return {
+              cwd = basedir,
+              search_dirs = { basedir },
+            }
+          end
+
+          vim.keymap.set("n", "f", function()
+            require("telescope.builtin").live_grep(get_telescope_opts())
+          end, opts("Find string in folder"))
+
+          vim.keymap.set("n", "F", function()
+            require("telescope.builtin").find_files(get_telescope_opts())
+          end, opts("Find file in folder"))
+        end,
+      })
     end,
   },
 }
