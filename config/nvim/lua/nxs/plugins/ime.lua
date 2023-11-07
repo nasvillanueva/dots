13c3927634f8861ec0614f6df50c01f8e9f6d119
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-11-07 15:01:16 +0900

    Add im-select

---
+++ config/nvim/lua/nxs/plugins/ime.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "keaising/im-select.nvim",
+    event = "VeryLazy",
+    main = "im_select",
+    opts = {
+      default_im_select = "com.apple.inputmethod.Kotoeri.RomajiTyping.Roman",
+    },
+  },
+}
