a0af9447b2ac06cb69d85febd7ef301713ecb51f
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-11-08 23:16:10 +0900

    Properly integrate lazygit and nvim

--- config/nvim/lua/nxs/lazygit_edit.lua
+++ config/nvim/lua/nxs/lazygit_edit.lua
@@ -1,6 +1,8 @@
+-- TODO: Figure out why line number is not working when opening in
+-- NVIM > LAZYGIT, but works with LAZYGIT alone
 return function(filename, line_number)
   line_number = tonumber(line_number) or 1
 
   vim.api.nvim_win_close(0, true)
-  vim.api.nvim_command("edit +" .. line_number .. " " .. filename)
+  vim.cmd("edit +" .. line_number .. " " .. filename)
 end
