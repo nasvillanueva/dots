fa181a94527d06ddc26226514480c001980f75b2
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-11-08 22:18:22 +0900

    Add script to edit files from lazygit

---
+++ config/nvim/lua/nxs/lazygit_edit.lua
@@ -0,0 +1,6 @@
+return function(filename, line_number)
+  line_number = tonumber(line_number) or 1
+
+  vim.api.nvim_win_close(0, true)
+  vim.api.nvim_command("edit +" .. line_number .. " " .. filename)
+end
