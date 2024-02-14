99271656312a07a8b367cb58ff8b24d5f077d2c0
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-02-15 00:01:19 +0900

    Set silent true to keybind options

--- config/nvim/lua/nxs/utils/keybind.lua
+++ config/nvim/lua/nxs/utils/keybind.lua
@@ -1,5 +1,5 @@
 return {
   set = function(mode, keybind, cmd, desc)
-    vim.keymap.set(mode, keybind, cmd, { desc = desc, noremap = true  })
+    vim.keymap.set(mode, keybind, cmd, { desc = desc, noremap = true, silent = true  })
   end
 }
