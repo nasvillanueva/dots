c8e2542df86e970207c0ba9ad57f86fd3bb269e7
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-02-15 00:05:04 +0900

    Update telescope config part 1

--- config/nvim/lua/nxs/telescope.lua
+++ config/nvim/lua/nxs/telescope.lua
@@ -15,15 +15,40 @@ local keybind = require("nxs.utils.keybind")
 local telescope = require("telescope")
 
 telescope.setup({
+  defaults = require("telescope.themes").get_ivy({
+    layout_strategy = "bottom_pane",
+    prompt_position = "bottom",
+    sorting_strategy = "descending",
+    border = false,
+    prompt_prefix = '➜ ',
+    selection_caret = '  ',
+  }),
   extensions = {
     fzf = {
       fuzzy = true,
       override_file_sorter = true,
       case_mode = "smart_case",
     }
-  }
+  },
 })
 
 telescope.load_extension("fzf")
 
-keybind.set("n", "<leader>O", ":Telescope find_files<CR>", "Telescope: Find Files")
+-- TODO: Move this to utils? I have to remove usage of telescope.utils though
+local function is_git_project()
+  local _, ret, _ = require("telescope.utils").get_os_command_output({
+    "git",
+    "rev-parse",
+    "--is-inside-work-tree",
+  })
+
+  return ret == 0
+end
+
+if is_git_project() then
+  keybind.set("n", "<leader>O", ":Telescope find_files<CR>", "Telescope: Find Files")
+else
+  keybind.set("n", "<leader>O", ":Telescope git_files<CR>", "Telescope: Git Files")
+end
+
+keybind.set("n", "<leader>e", ":Telescope oldfiles<CR>", "Telescope: Recent Files")
