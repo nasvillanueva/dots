871c8e24dbdaf8a610d3d7a5f3890608f52f2ae8
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-02-21 13:19:36 +0900

    Add lualine

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -13,6 +13,7 @@ require("nxs.keybinds")
 -- Needed by many dependencies
 require("nvim-web-devicons").setup({})
 
+require("nxs.lualine")
 require("nxs.explorer")
 require("nxs.ime")
 require("nxs.treesitter")
