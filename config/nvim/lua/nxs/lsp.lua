f9688e6cc80d212f2cc830cee95a21d5dc9d2517
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-03-11 23:13:27 +0900

    Configure lsp

--- config/nvim/lua/nxs/lsp.lua
+++ config/nvim/lua/nxs/lsp.lua
@@ -62,6 +62,24 @@ local lspconfig = require("lspconfig")
 local get_mason_servers = require("mason-lspconfig").get_installed_servers
 
 local NXS_LSP_CONFIG = {
+  cssls = {},
+  tailwindcss = {
+    filetypes = { "vue", "html", "css", "scss" },
+  },
+  elixirls = {
+    cmd = {
+      os.getenv("HOME")
+        .. "/.local/share/nvim/mason/packages/elixir-ls/language_server.sh",
+    },
+  },
+  eslint = {
+    on_attach = function(_, bufnr)
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        command = "EslintFixAll",
+      })
+    end,
+  },
   lua_ls = {
     settings = {
       Lua = {
@@ -80,6 +98,41 @@ local NXS_LSP_CONFIG = {
       },
     },
   },
+  tsserver = {
+    on_attach = function()
+      keybind.set("", "<leader>co", ":OrganizeImports<CR>")
+    end,
+    commands = {
+      OrganizeImports = {
+        function()
+          vim.lsp.buf.execute_command({
+            command = "_typescript.organizeImports",
+            arguments = { vim.api.nvim_buf_get_name(0) },
+            title = "",
+          })
+        end,
+        description = "Organize Imports",
+      },
+    },
+  },
+  volar = {
+    filetypes = { "vue", "typescript" },
+    settings = {
+      scss = {
+        lint = {
+          unknownAtRules = "ignore",
+        },
+      },
+    },
+  },
+  jsonls = { filetypes = { "json", "jsonc" } },
+  stylelint_lsp = {
+    settings = {
+      stylelintplus = {
+        autoFixOnFormat = true,
+      },
+    },
+  },
 }
 
 for _, server_name in ipairs(get_mason_servers()) do
@@ -89,3 +142,70 @@ for _, server_name in ipairs(get_mason_servers()) do
     capabilities = lsp_capabilities,
   }, nxs_lsp_config))
 end
+
+-- Volar Takeover mode
+-- https://vuejs.org/guide/typescript/overview.html#volar-takeover-mode
+local volar_client = nil
+local tsserver_client = nil
+local function volar_takeover(args)
+  local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+  if client.name == "tsserver" then
+    if volar_client then
+      client:stop()
+    else
+      tsserver_client = client
+    end
+  elseif client.name == "volar" then
+    volar_client = client
+
+    if tsserver_client then
+      tsserver_client:stop()
+    end
+  end
+end
+
+local function setup_keybindings(args)
+  keybind.set("n", "<leader>lsp", ":silent :LspRestart<CR>", "LSP: Restart")
+  vim.keymap.set(
+    "n",
+    "<leader>ck",
+    vim.lsp.buf.hover,
+    { buffer = args.buf, desc = "LSP: Hover" }
+  )
+  keybind.set("n", "<leader>ca", vim.lsp.buf.code_action, "LSP: Code Actions")
+  keybind.set("n", "<leader>cr", vim.lsp.buf.rename, "LSP: Rename")
+  keybind.set(
+    "n",
+    "<leader>cd",
+    vim.lsp.buf.definition,
+    "LSP: Go to Definition"
+  )
+  keybind.set("n", "<leader>cu", vim.lsp.buf.references, "LSP: Find Usages")
+  keybind.set(
+    "n",
+    "<leader>ch",
+    vim.lsp.buf.signature_help,
+    "LSP: Show Signature Help"
+  )
+  keybind.set("n", "<leader>ce", vim.diagnostic.open_float, "LSP: View Error")
+  keybind.set(
+    "n",
+    "<leader>c[",
+    vim.diagnostic.goto_prev,
+    "LSP: Goto Previous Diagnostic Error"
+  )
+  keybind.set(
+    "n",
+    "<leader>c]",
+    vim.diagnostic.goto_next,
+    "LSP: Goto Next Diagnostic Error"
+  )
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    volar_takeover(args)
+    setup_keybindings(args)
+  end,
+})
