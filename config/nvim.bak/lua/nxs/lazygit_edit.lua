8b72339b7d8528693fcdda451e9b60837373b537
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-03-18 21:39:18 +0900

    Delete old nvim config

--- config/nvim.bak/lua/nxs/lazygit_edit.lua
+++
@@ -1,8 +0,0 @@
--- TODO: Figure out why line number is not working when opening in
--- NVIM > LAZYGIT, but works with LAZYGIT alone
-return function(filename, line_number)
-  line_number = tonumber(line_number) or 1
-
-  vim.api.nvim_win_close(0, true)
-  vim.cmd("edit +" .. line_number .. " " .. filename)
-end
--- config/nvim.bak2/lua/nxs/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-vim.g.nord_contrast = true
-vim.g.nord_uniform_diff_background = true
-vim.g.nord_disable_background = true
-vim.g.nord_borders = true
-
-require("nord").set()
-vim.opt.termguicolors = true
