8d1bcc640849f5a6f7435b0e263579dafeb5a6bd
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-04-15 14:02:00 +0900

    Fix formatter

--- config/nvim/lua/nxs/autocmd/formatter.lua
+++ config/nvim/lua/nxs/autocmd/formatter.lua
@@ -1,5 +1,3 @@
-local lsp = require("nxs.utils.lsp")
-
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = { "*.js", "*.cjs", "*.mjs", "*.jsx", "*.ts", "*.tsx", "*.vue" },
   callback = function()
@@ -10,9 +8,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function(args)
-    if lsp.supports("textDocument/formatting", args.buf) then
-      vim.lsp.buf.format({ async = true })
-    end
+  pattern = { "*.ex", "*.exs", "*.heex" },
+  callback = function()
+    vim.lsp.buf.format({ async = true })
   end,
 })
