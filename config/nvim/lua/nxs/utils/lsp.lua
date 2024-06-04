889dfe533bed4ef52d24304b8eede2a51cd6a167
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-06-04 23:53:32 +0900

    Replace lsp util with 0.10 feature

--- config/nvim/lua/nxs/utils/lsp.lua
+++ config/nvim/lua/nxs/utils/lsp.lua
@@ -1,8 +1,6 @@
 return {
   supports = function(method, bufnr)
-    -- Only on nvim 0.10.0
-    -- local clients = vim.lsp.get_clients({ bufnr = args.buf })
-    local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+    local clients = vim.lsp.get_clients({ bufnr = bufnr })
 
     for _, client in ipairs(clients) do
       if client.supports_method(method) then
