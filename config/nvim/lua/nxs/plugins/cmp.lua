808143e8a1e5b21c1c2fdb0667ce9a490a233268
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-06-06 11:54:09 +0900

    Hide copilot when cmp is opened

--- config/nvim/lua/nxs/plugins/cmp.lua
+++ config/nvim/lua/nxs/plugins/cmp.lua
@@ -84,6 +84,14 @@ return {
 
       local cmp_autopairs = require("nvim-autopairs.completion.cmp")
       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+      -- Disables copilot suggestions when cmp menu is open
+      cmp.event:on("menu_opened", function()
+        vim.b.copilot_suggestion_hidden = true
+      end)
+      cmp.event:on("menu_closed", function()
+        vim.b.copilot_suggestion_hidden = false
+      end)
     end,
   },
 }
