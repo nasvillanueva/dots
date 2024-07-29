932ad72169ab32be7c1dc4966ec31f6e7f7b18d6
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-07-29 17:45:17 +0900

    Remove name for colorscheme

--- config/nvim/lua/nxs/plugins/colorscheme.lua
+++ config/nvim/lua/nxs/plugins/colorscheme.lua
@@ -2,7 +2,6 @@ return {
  "diegoulloao/neofusion.nvim",
   lazy = false,
   priority = 1000,
-  name = "rose-pine",
   init = function()
     vim.o.background = "dark"
     vim.cmd("colorscheme neofusion")
