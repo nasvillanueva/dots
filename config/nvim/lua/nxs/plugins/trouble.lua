c3c311f660086889c6b92fed1742d67c848b1df8
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-09-02 00:45:07 +0900

    Fix trouble

--- config/nvim/lua/nxs/plugins/trouble.lua
+++ config/nvim/lua/nxs/plugins/trouble.lua
@@ -4,36 +4,40 @@ return {
   {
     "folke/trouble.nvim",
     cmd = { "TroubleToggle", "Trouble" },
-    opts = { use_diagnostic_signs = true, auto_close = true, auto_preview = false },
+    opts = {
+      use_diagnostic_signs = true,
+      auto_close = true,
+      auto_preview = false,
+    },
     keys = {},
     init = function()
       keybind.set(
         "n",
         "<leader>xx",
-        "<cmd>TroubleToggle document_diagnostics<cr>",
+        "<cmd>Trouble diagnostics toggle<cr>",
         "Document Diagnostics (Trouble)"
       )
       keybind.set(
         "n",
         "<leader>xX",
-        "<cmd>TroubleToggle workspace_diagnostics<cr>",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
         "Workspace Diagnostics (Trouble)"
       )
       keybind.set(
         "n",
         "<leader>xL",
-        "<cmd>TroubleToggle loclist<cr>",
+        "<cmd>Trouble loclist toggle<cr>",
         "Location List (Trouble)"
       )
       keybind.set(
         "n",
         "<leader>xQ",
-        "<cmd>TroubleToggle quickfix<cr>",
+        "<cmd>Trouble qflist toggle<cr>",
         "Quickfix List (Trouble)"
       )
       keybind.set("n", "[q", function()
         if require("trouble").is_open() then
-          require("trouble").previous({ skip_groups = true, jump = true })
+          require("trouble").prev({ skip_groups = true, jump = true })
         else
           local ok, err = pcall(vim.cmd.cprev)
           if not ok then
@@ -60,13 +64,26 @@ return {
       signs = false,
     },
     init = function()
-      keybind.set( "n", "]t", function() require("todo-comments").jump_next() end, "Next todo comment" )
-      keybind.set( "n", "[t", function() require("todo-comments").jump_prev() end, "Previous todo comment" )
-      keybind.set( "n", "<leader>xt", "<cmd>TodoTrouble<cr>", "Todo (Trouble)" )
-      keybind.set( "n", "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", "Todo/Fix/Fixme (Trouble)" )
-      keybind.set( "n", "<leader>st", "<cmd>TodoTelescope<cr>", "Todo" )
-      keybind.set( "n", "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", "Todo/Fix/Fixme" )
-    end
-
+      keybind.set("n", "]t", function()
+        require("todo-comments").jump_next()
+      end, "Next todo comment")
+      keybind.set("n", "[t", function()
+        require("todo-comments").jump_prev()
+      end, "Previous todo comment")
+      keybind.set("n", "<leader>xt", "<cmd>TodoTrouble<cr>", "Todo (Trouble)")
+      keybind.set(
+        "n",
+        "<leader>xT",
+        "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",
+        "Todo/Fix/Fixme (Trouble)"
+      )
+      keybind.set("n", "<leader>st", "<cmd>TodoTelescope<cr>", "Todo")
+      keybind.set(
+        "n",
+        "<leader>sT",
+        "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",
+        "Todo/Fix/Fixme"
+      )
+    end,
   },
 }
