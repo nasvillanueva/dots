4c233a5ec9bb6b3a044bc3eb33369427dc73e63b
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-09-06 08:48:57 +0900

    Update lualine

--- config/nvim/lua/nxs/plugins/lualine.lua
+++ config/nvim/lua/nxs/plugins/lualine.lua
@@ -16,32 +16,9 @@ return {
           },
           lualine_x = {
             "branch",
-            {
-              function()
-                local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-                local clients = vim.lsp.get_active_clients()
-
-                if next(clients) == nil then
-                  return buf_ft
-                end
-
-                local active_lsp = {}
-                for _, client in ipairs(clients) do
-                  local filetypes = client.config.filetypes
-                  if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                    table.insert(active_lsp, client.name)
-                  end
-                end
-
-                if #active_lsp == 0 then
-                  return buf_ft
-                end
-
-                return buf_ft .. ": " .. table.concat(active_lsp, ", ")
-              end,
-              icon = "",
-            },
+            "filetype",
           },
+          lualine_y = {},
         },
       })
     end,
