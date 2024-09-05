e9df749271616493dccc33488df8ce85d821a9c4
Nas Villanueva <jonas.villanueva@restar-inc.com>
2024-09-06 08:54:23 +0900

    Handle renaming of tsserver to ts_ls

--- config/nvim/lua/nxs/plugins/lsp/init.lua
+++ config/nvim/lua/nxs/plugins/lsp/init.lua
@@ -166,7 +166,14 @@ return {
       for _, server_name in ipairs(get_mason_servers()) do
         local nxs_lsp_config = configured_servers[server_name] or {}
 
-        lspconfig[server_name].setup(vim.tbl_deep_extend("force", {
+        local lsp_server_name = server_name
+
+        -- temporarily rename tsserver to ts_ls because of an update
+        if server_name == "tsserver" then
+          lsp_server_name = "ts_ls"
+        end
+
+        lspconfig[lsp_server_name].setup(vim.tbl_deep_extend("force", {
           capabilities = lsp_capabilities,
         }, nxs_lsp_config))
       end
