01f96c416c5ed2f7dcf576436463d914b62343da
Nas Villanueva <jonas.villanueva@restar-inc.com>
2025-01-23 21:47:45 +0900

    Add friendly snippets

--- config/nvim/lua/nxs/plugins/luasnip.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "L3MON4D3/LuaSnip",
-  event = "InsertEnter",
-  version = "v2.*",
-  build = "make install_jsregexp",
-}
---
+++ config/nvim/lua/nxs/plugins/snippets.lua
@@ -0,0 +1,9 @@
+return {
+  "L3MON4D3/LuaSnip",
+  version = "v2.*",
+  build = "make install_jsregexp",
+  dependencies = { "rafamadriz/friendly-snippets" },
+  init = function()
+    require("luasnip.loaders.from_vscode").lazy_load()
+  end,
+}
