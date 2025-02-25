8560b7905f161ecd310b67680346a6fc7a55fe81
Nas Villanueva <jonas.villanueva@restar-inc.com>
2025-02-25 15:56:17 +0900

    Remove nvim tree

--- config/nvim/lua/nxs/plugins/files.lua
+++ config/nvim/lua/nxs/plugins/files.lua
@@ -19,39 +19,4 @@ return {
       keybind.set("n", "<leader>e", "<CMD>Oil<CR>", "File Explorer")
     end,
   },
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      require("nvim-tree").setup({
-        renderer = {
-          group_empty = true,
-        },
-        view = {
-          float = {
-            enable = true,
-          },
-        },
-      })
-
-      keybind.set(
-        "n",
-        "<leader>E",
-        "<CMD>NvimTreeOpen<CR>",
-        "File Explorer: Tree"
-      )
-
-      keybind.set(
-        "n",
-        "<leader>fE",
-        ":NvimTreeFindFile<CR>",
-        "Explorer: Focus File"
-      )
-    end,
-  },
 }
