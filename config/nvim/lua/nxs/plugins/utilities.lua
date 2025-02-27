daf97cc242bbfcd9725bbb04d849ecbfd769a3dc
Nas Villanueva <jonas.villanueva@restar-inc.com>
2025-02-27 10:56:11 +0900

    Disable nvim autopairs for snacks_picker_input

--- config/nvim/lua/nxs/plugins/utilities.lua
+++ config/nvim/lua/nxs/plugins/utilities.lua
@@ -50,7 +50,9 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    config = true,
+    opts = {
+      disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
+    },
   },
   {
     "nvim-pack/nvim-spectre",
