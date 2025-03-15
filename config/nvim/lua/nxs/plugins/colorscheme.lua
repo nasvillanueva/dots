dec0878aad7d82a68be09d5ed4c71b7a48ef7b73
Nas Villanueva <nasvillanuevait@gmail.com>
2025-03-15 18:33:47 +0900

    Update NonText Highlights

--- config/nvim/lua/nxs/plugins/colorscheme.lua
+++ config/nvim/lua/nxs/plugins/colorscheme.lua
@@ -3,7 +3,11 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require("neofusion").setup()
+    require("neofusion").setup({
+      overrides = {
+        NonText = { link = "NeofusionGray" }
+      }
+    })
 
     vim.o.background = "dark"
     vim.cmd("colorscheme neofusion")
