60b1143507af01fe0cd02b0d4f54a374457944ee
Nas Villanueva <nasvillanuevait@gmail.com>
2025-03-20 17:34:44 +0900

    Fix snacks picker colors

--- config/nvim/lua/nxs/plugins/colorscheme.lua
+++ config/nvim/lua/nxs/plugins/colorscheme.lua
@@ -7,6 +7,12 @@ return {
     require("rose-pine").setup({
       variant = "dawn",
       extend_background_behind_borders = false,
+      highlight_groups = {
+        SnacksPickerInput = { bg = "base" },
+        SnacksPickerTitle = { bg = "base" },
+        SnacksPickerList = { bg = "base" },
+        SnacksPickerPreview = { bg = "base" },
+      },
     })
 
     vim.o.background = "light"
