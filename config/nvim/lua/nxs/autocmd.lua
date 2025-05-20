fa6360a8af10af092aabb42a189aca55c8766e1b
Nas Villanueva <nasvillanuevait@gmail.com>
2025-05-20 18:06:07 +0900

    Remove checktime autocmd

--- config/nvim/lua/nxs/autocmd.lua
+++ config/nvim/lua/nxs/autocmd.lua
@@ -52,16 +52,6 @@ vim.api.nvim_create_autocmd("WinResized", {
   end,
 })
 
--- Check if we need to reload the file when it changed
-vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-  group = augroup("checktime"),
-  callback = function()
-    if vim.o.buftype ~= "nofile" then
-      vim.cmd("checktime")
-    end
-  end,
-})
-
 -- resize splits if window got resized
 vim.api.nvim_create_autocmd({ "VimResized" }, {
   group = augroup("resize_splits"),
