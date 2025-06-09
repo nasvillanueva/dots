d4b6bfd4ae8bc5209e4c6d13a7867fd7eca6039d
Nas Villanueva <nasvillanuevait@gmail.com>
2025-06-09 16:02:55 +0900

    Bring back autocmd for checktime

--- config/nvim/lua/nxs/autocmd.lua
+++ config/nvim/lua/nxs/autocmd.lua
@@ -83,3 +83,13 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup("checktime"),
+  callback = function()
+    if vim.o.buftype ~= "nofile" then
+      vim.cmd("checktime")
+    end
+  end,
+})
