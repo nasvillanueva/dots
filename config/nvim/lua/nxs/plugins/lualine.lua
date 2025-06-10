79fea042d61c611cf4aa233e8691ea0cb5693633
Nas Villanueva <nasvillanuevait@gmail.com>
2025-06-10 14:34:25 +0900

    Use default lualine settings

--- config/nvim/lua/nxs/plugins/lualine.lua
+++ config/nvim/lua/nxs/plugins/lualine.lua
@@ -1,9 +1,3 @@
-local icon_by_string = {
-  ["oil:"] = "",
-  ["term:"] = "",
-  ["NvimTree_1"] = "",
-}
-
 return {
   {
     "nvim-lualine/lualine.nvim",
@@ -11,51 +5,7 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("lualine").setup({
-        options = {
-          component_separators = "",
-          section_separators = "",
-        },
-        sections = {
-          lualine_b = { "diagnostics" },
-          lualine_c = {},
-          lualine_x = {
-            {
-              function()
-                local buf_ft = vim.api.nvim_get_option_value("filetype", {
-                  buf = 0,
-                })
-                local clients = vim.lsp.get_clients()
-
-                if next(clients) == nil then
-                  return buf_ft
-                end
-
-                local active_lsp = {}
-                for _, client in ipairs(clients) do
-                  if client.name == "copilot" then
-                    table.insert(active_lsp, "copilot")
-                  end
-
-                  local filetypes = client.config.filetypes
-                  if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                    table.insert(active_lsp, client.name)
-                  end
-                end
-                if #active_lsp == 0 then
-                  return buf_ft
-                end
-
-                return buf_ft .. ": " .. table.concat(active_lsp, ", ")
-              end,
-              icon = "",
-            },
-            "branch",
-          },
-          lualine_y = {},
-        },
-        inactive_sections = {},
-      })
+      require("lualine").setup({})
     end,
   },
 }
