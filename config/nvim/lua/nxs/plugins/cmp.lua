30e7227e8653eb09313afb89cdbd1e646e09ee41
Nas Villanueva <nasvillanuevait@gmail.com>
2025-06-14 23:28:40 +0900

    add lspkind to cmp

--- config/nvim/lua/nxs/plugins/cmp.lua
+++ config/nvim/lua/nxs/plugins/cmp.lua
@@ -4,9 +4,14 @@ return {
     version = "1.*",
     dependencies = {
       "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
+      "nvim-tree/nvim-web-devicons",
     },
     config = function()
       local blink = require("blink-cmp")
+      local lspkind = require("lspkind")
+      local devicons = require("nvim-web-devicons")
+
       blink.setup({
         keymap = {
           preset = "none",
@@ -112,6 +117,35 @@ return {
             draw = {
               treesitter = {
                 "lsp",
+              },
+              components = {
+                kind_icon = {
+                  text = function(ctx)
+                    local icon = ctx.kind_icon
+                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                      local dev_icon, _ = devicons.get_icon(ctx.label)
+                      if dev_icon then
+                        icon = dev_icon
+                      end
+                    else
+                      icon = lspkind.symbolic(ctx.kind, {
+                        mode = "symbol",
+                      })
+                    end
+
+                    return icon .. ctx.icon_gap
+                  end,
+                  highlight = function(ctx)
+                    local hl = ctx.kind_hl
+                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                      local dev_icon, dev_hl = devicons.get_icon(ctx.label)
+                      if dev_icon then
+                        hl = dev_hl
+                      end
+                    end
+                    return hl
+                  end,
+                },
               },
             },
           },
