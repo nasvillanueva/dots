26bc9774c1c731a02554d4cd7fd9726c5d1639a3
Nas Villanueva <nasvillanuevait@gmail.com>
2026-04-10 19:09:58 +0900

    Add ai stuff

---
+++ config/nvim/plugin/41_ai.lua
@@ -0,0 +1,37 @@
+local setup_deferred = _G.nxs.deferred_packadd({
+  _G.nxs.gh("zbirenbaum/copilot.lua"),
+  {
+    src = _G.nxs.gh("olimorris/codecompanion.nvim"),
+    version = vim.version.range("^19.0.0"),
+  },
+})
+
+setup_deferred(function()
+  require("copilot").setup({
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+  })
+
+  require("codecompanion").setup({
+    chat = {
+      adapter = "copilot",
+    },
+    inline = {
+      adapter = "copilot",
+    },
+    cmd = {
+      adapter = "copilot",
+    },
+    background = {
+      adapter = "copilot",
+    },
+  })
+
+  _G.nxs.keybind_set("n", "<leader>ait", "<cmd>CodeCompanionChat Toggle<cr>")
+  _G.nxs.keybind_set({ "n", "v" }, "<leader>aic", "<cmd>CodeCompanion<cr>")
+  _G.nxs.keybind_set(
+    { "n", "v" },
+    "<leader>aig",
+    "<cmd>CodeCompanionActions<cr>"
+  )
+end)
