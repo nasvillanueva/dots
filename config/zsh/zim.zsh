72ed10c608e1bcc6258f76bfa1c2c0875dcd2cf7
Nas Villanueva <nasvillanuevait@gmail.com>
2026-08-22 13:34:42 +0900

    Add FZF rose pine theme

--- config/zsh/zim.zsh
+++ config/zsh/zim.zsh
@@ -1,3 +1,12 @@
+export FZF_DEFAULT_OPTS="
+	--color=fg:#797593,bg:#faf4ed,hl:#d7827e
+	--color=fg+:#575279,bg+:#f2e9e1,hl+:#d7827e
+	--color=border:#dfdad9,header:#286983,gutter:#faf4ed
+	--color=spinner:#ea9d34,info:#56949f
+	--color=pointer:#907aa9,marker:#b4637a,prompt:#797593"
+
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
+
 # LSPs like expert-lsp seems to fail to load if fzf is installed
 # and since it's not possible to conditionally load a zmodule in zimrc
 # this prevents loading the whole zimfw when the shell is not interactive
